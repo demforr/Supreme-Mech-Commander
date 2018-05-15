2235b3bd4ac7276b590015c5fab30fd516939681
Kamil Micek <kamisama.demforr@gmail.com>
2018-05-15 19:29:40 -0400

    Replaced FS Von Luckner k65n with k100, added new mesh for vnlk100.

--- units/FSVNLK100/FSVNLK100_Script.lua
+++ units/FSVNLK100/FSVNLK100_Script.lua
@@ -1,25 +1,21 @@
 #****************************************************************************
 #**
-#**  File     :  /Supreme Mech Commander/units/FSVNLK65N/FSVNLK65N_script.lua
+#**  File     :  /Supreme Mech Commander/units/FSVNLK100/FSVNLK100_script.lua
 #**  Author(s):  John Comes, David Tomandl, Jessica St. Croix, Kami, FuegoTigre
 #**
-#**  Summary  :  FedSuns Von Luckner VNL-K65N Heavy Tank
+#**  Summary  :  FedSuns Von Luckner VNL-K100 Heavy Tank
 #**
 #**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
 #****************************************************************************
 local TLandUnit = import('/lua/terranunits.lua').TLandUnit
 local TDFGaussCannonWeapon = import('/lua/terranweapons.lua').TDFGaussCannonWeapon
-local TDFMachineGunWeapon = import('/lua/terranweapons.lua').TDFMachineGunWeapon
 local TSAMLauncher = import('/lua/terranweapons.lua').TSAMLauncher
 
-FSVNLK65N = Class(TLandUnit) {
+FSVNLK100 = Class(TLandUnit) {
     Weapons = {
-    	MG = Class(TDFMachineGunWeapon) {},
-    	FLAMER = Class(TDFMachineGunWeapon) {},
-    	LRM = Class(TSAMLauncher) {},
     	SRM = Class(TSAMLauncher) {},
     	AC20 = Class(TDFGaussCannonWeapon) {},
     },
 }
 
-TypeClass = FSVNLK65N
+TypeClass = FSVNLK100
