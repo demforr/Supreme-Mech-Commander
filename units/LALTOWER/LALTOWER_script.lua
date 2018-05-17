4f2d30ba8d5e7618eecbd13b56c0fe8aba270743
Kamil Micek <kamisama.demforr@gmail.com>
2018-05-17 00:53:21 -0400

    Uvmaped Gazzel and textured, updated texture on leo.

---
+++ units/LALTOWER/LALTOWER_script.lua
@@ -0,0 +1,17 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/LALTOWER/LALTOWER_script.lua
+#**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
+#**
+#**  Summary  :  Light Radar Tower Script
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local TRadarUnit = import('/lua/terranunits.lua').TRadarUnit
+
+LALTOWER = Class(TRadarUnit) {
+
+}
+
+TypeClass = LALTOWER
