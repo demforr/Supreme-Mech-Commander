cfb09e8ce2ed6358e28642e74cd93ddc0ced5205
Kamil Micek <kamisama.demforr@gmail.com>
2018-06-05 01:16:11 -0400

    LAPPR5T addtion.
    Added Salamander.

--- units/LAPPR5T/LAPPR5T_Script.lua
+++ units/LAPPR5T/LAPPR5T_Script.lua
@@ -7,7 +7,6 @@ LAPPR5T = Class(TWalkingLandUnit) {
     Weapons = {
     	LRM01 = Class(TSAMLauncher) {},
     	LRM02 = Class(TSAMLauncher) {},
-    	LRM03 = Class(TSAMLauncher) {},
         SL01 = Class(ADFLaserLightWeapon) {},
         SL02 = Class(ADFLaserLightWeapon) {},
     },
