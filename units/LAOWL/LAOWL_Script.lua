b57c165d6387d3054c7539d74fded876c40990f8
Kamil Micek <kamisama.demforr@gmail.com>
2018-08-20 01:22:10 -0400

    Comet Owl added

---
+++ units/LAOWL/LAOWL_Script.lua
@@ -0,0 +1,19 @@
+#****************************************************************************
+
+local TAirUnit = import('/lua/terranunits.lua').TAirUnit
+local TSAMLauncher = import('/lua/terranweapons.lua').TSAMLauncher
+local TIFCarpetBombWeapon = import('/lua/terranweapons.lua').TIFCarpetBombWeapon
+
+
+LAOWL = Class(TAirUnit) {
+    PlayDestructionEffects = true,
+    DamageEffectPullback = 0.25,
+    DestroySeconds = 7.5,
+
+    Weapons = {
+        SRM = Class(TSAMLauncher) {},
+        Bomb = Class(TIFCarpetBombWeapon) {},
+    },
+}
+
+TypeClass = LAOWL
