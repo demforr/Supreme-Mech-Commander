54af7d6e674572da862c5a35c0afd47a119deee8
Kamil Micek <kamisama.demforr@gmail.com>
2018-10-01 02:16:59 -0400

    Texture updates

--- units/LAHBK4G/LAHBK4G_Script.lua
+++ units/LAHBK4G/LAHBK4G_Script.lua
@@ -1,19 +1,9 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/FSUZL2S/FSUZL2S_script.lua
-#**  Author(s):  John Comes, David Tomandl, Jessica St. Croix, Kami, FuegoTigre
-#**
-#**  Summary  :  Davion Hollander II 
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
-
 local TerranWeaponFile = import('/lua/terranweapons.lua')
 local TWalkingLandUnit = import('/lua/terranunits.lua').TWalkingLandUnit
 local TDFGaussCannonWeapon = import('/lua/terranweapons.lua').TDFGaussCannonWeapon
 local ADFLaserLightWeapon = import('/lua/aeonweapons.lua').ADFLaserLightWeapon
 
-FSHBK4G = Class(TWalkingLandUnit) {
+LAHBK4G = Class(TWalkingLandUnit) {
     Weapons = {
         AC20 = Class(TDFGaussCannonWeapon) {},  
         ML01 = Class(ADFLaserLightWeapon) {},
@@ -22,4 +12,4 @@ FSHBK4G = Class(TWalkingLandUnit) {
     },
 }
 
-TypeClass = FSHBK4G
+TypeClass = LAHBK4G
