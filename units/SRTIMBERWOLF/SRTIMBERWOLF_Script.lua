70ec3f797ac8ac0c749ed05b59ba5f91c1817b28
Kamil Micek <kamisama.demforr@gmail.com>
2018-10-12 00:03:17 -0400

    TW files

--- units/SRTIMBERWOLF/SRTIMBERWOLF_Script.lua
+++ units/SRTIMBERWOLF/SRTIMBERWOLF_Script.lua
@@ -1,14 +1,19 @@
 local TWalkingLandUnit = import('/lua/terranunits.lua').TWalkingLandUnit
-local CDFParticleCannonWeapon = import('/lua/cybranweapons.lua').CDFParticleCannonWeapon
 local TSAMLauncher = import('/lua/terranweapons.lua').TSAMLauncher
+local CDFParticleCannonWeapon = import('/lua/cybranweapons.lua').CDFParticleCannonWeapon
+local ADFLaserLightWeapon = import('/lua/aeonweapons.lua').ADFLaserLightWeapon
+local TDFMachineGunWeapon = import('/lua/terranweapons.lua').TDFMachineGunWeapon
 
 
-SRMADDOG = Class(TWalkingLandUnit) {
+SRTIMBERWOLF = Class(TWalkingLandUnit) {
     Weapons = {
 		CLRM = Class(TSAMLauncher) {},
+    	CERLL = Class(ADFLaserLightWeapon) {},
+		CERML = Class(ADFLaserLightWeapon) {},
     	CMPL = Class(CDFParticleCannonWeapon) {},
-    	CLPL = Class(CDFParticleCannonWeapon) {},
+    	MG1 = Class(TDFMachineGunWeapon) {},
+    	MG2 = Class(TDFMachineGunWeapon) {},
     },
 }
 
-TypeClass = SRMADDOG
+TypeClass = SRTIMBERWOLF
