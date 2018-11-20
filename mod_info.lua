4b41998fd6dd3199b26a8104adf0d472f3b597a3
Kamil Micek <kamisama.demforr@gmail.com>
2018-11-20 17:27:44 -0500

    mod info updated

--- mod_info.lua
+++ mod_info.lua
@@ -1,7 +1,7 @@
 name = "Supreme Mech Commander"
-version = 0.6
+version = 0.7
 copyright = "Copyright © 2006, Gas Powered Games"
-description = "Supreme Mech Commander v0.6"
+description = "Supreme Mech Commander v0.7"
 author = "Kamisama171 and FuegoTigre"
 url = "http://www.moddb.com/mods/supreme-mech-commander"
 icon = "/Mods/Supreme Mech Commander/Mini_icon.jpg"
